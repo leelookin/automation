12ea50e5c24d08470e7d1bc7f8e4765539f540fe
Aleh_Khadyka <Aleh_Khadyka@epam.com>
2017-04-05 12:38:04 -0400

    My first commit!

---
+++ NET/Calc2/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Calc2
+{
+    public static class Calculator
+    {
+        public static double Plus(double x1, double x2) {
+            return x1 + x2;
+        }
+
+        public static double Minus(double x1, double x2) {
+            return x1 - x2;
+        }
+
+        public static double Divide(double x1, double x2) {
+            return x1 / x2;
+        }
+
+        public static double Multiply(double x1, double x2) {
+            return x1 * x2;
+        }
+
+        public static double Balance(double x1, double x2) {
+            return x1 % x2;
+        }
+
+        public static double Sqrt(double x1) {
+            return Math.Sqrt(x1);
+        }
+    }
+}
---
+++ NET/Calc2/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Calc2
+{
+    class Program
+    {
+        public static double x1, x2;
+        public static string _operator;
+        
+
+        static void Main(string[] args)
+        {
+            var isActive = true;
+            double result = 0;
+
+            while (isActive) {
+
+                // call method which reads keys from console
+                ReadValues();              
+
+                switch (_operator)
+                {
+                    case "+":
+                        result = Calculator.Plus(x1, x2);
+                        break;
+                    case "-":
+                        result = Calculator.Minus(x1, x2);
+                        break;
+                    case "/":
+                        result = Calculator.Divide(x1, x2);
+                        break;
+                    case "*":
+                        result = Calculator.Multiply(x1, x2);
+                        break;
+                    case "%":
+                        result = Calculator.Balance(x1, x2);
+                        break;
+                    case "#":
+                        result = Calculator.Sqrt(x1);
+                        break;
+                }
+
+                Console.WriteLine(result);  
+              
+                Console.WriteLine("Do you want to proceed? Type Y if yes");
+                var key = Console.ReadLine();
+
+                if (key.ToUpper() != "Y".ToUpper())
+                {
+                    isActive = false;
+                }
+
+                //TODO: use dictionary
+                // dictionary[_operator].GetResult(x1, x2);
+            }
+        }
+        
+        private static void ReadValues()
+        {
+            Console.WriteLine("Input the first number and click on Enter: ");
+
+            x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Would you like Count the sqrt or some thing elese? Type 'R' if root ");
+
+            var key = Console.ReadLine();
+
+            if (key.ToUpper() == "R".ToUpper())
+            {
+
+                _operator = "#";
+                return;
+                
+            }
+            else
+            {
+                Console.WriteLine("Input an operator '+', '-', '/', '*', '%' and click on Enter: ");
+
+                _operator = Console.ReadLine();
+
+                Console.WriteLine("Input the second number and click on Enter: ");
+
+                x2 = Convert.ToDouble(Console.ReadLine());
+            }         
+
+           
+        }
+
+    }
+
+}
+
+
+
+
